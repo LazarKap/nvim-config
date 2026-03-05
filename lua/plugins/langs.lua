c30b53bb35136e5e5206371426113725f67e74af
lazar.bogosavljevic <lazar.bogosavljevic@zendesk.com>
2026-03-05 15:01:41 +0100

    Add lsp

--- lua/plugins/langs.lua
+++ lua/plugins/langs.lua
@@ -1,2 +1,160 @@
--- Language extras are imported in lua/config/lazy.lua to ensure correct load order
-return {}
+return {
+  -- Configure mason-lspconfig to install specific language servers
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",           -- Go
+        "pyright",         -- Python
+        "ts_ls",           -- TypeScript/JavaScript
+        "intelephense",    -- PHP
+      },
+    },
+  },
+
+  -- Ensure language servers are installed via Mason
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "gopls",
+        "pyright", 
+        "typescript-language-server",
+        "intelephense",
+      })
+    end,
+  },
+
+  -- Configure LSP servers
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- Go LSP
+        gopls = {
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = true,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = true,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+
+        -- Python LSP  
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true,
+              },
+            },
+          },
+        },
+
+        -- TypeScript/JavaScript LSP
+        ts_ls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+
+        -- PHP LSP
+        intelephense = {
+          settings = {
+            intelephense = {
+              files = {
+                maxSize = 5000000,
+                associations = { "*.php", "*.phtml" },
+                exclude = {
+                  "**/node_modules/**",
+                  "**/vendor/**",
+                  "**/storage/**",
+                  "**/bootstrap/cache/**",
+                  "**/public/storage/**"
+                }
+              },
+              environment = {
+                includePaths = {
+                  "vendor/"
+                }
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  -- Treesitter support for all languages
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gowork",
+        "python",
+        "typescript",
+        "javascript",
+        "tsx",
+        "php",
+        "phpdoc",
+      })
+    end,
+  },
+}
