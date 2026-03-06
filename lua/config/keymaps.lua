6151c40ba62f16b41d08914e252024efa04d2858
lazar.bogosavljevic <lazar.bogosavljevic@zendesk.com>
2026-03-06 16:52:01 +0100

    Remove unused keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,16 +4,9 @@
 -- :wq closes current buffer instead of quitting nvim
 vim.cmd("cabbrev wq bd")
 
--- Close buffer with Ctrl+w (familiar shortcut)
-vim.keymap.set("n", "<C-w>", "<cmd>bd<cr>", { desc = "Close buffer" })
-
 -- Move between buffers with Tab / Shift+Tab
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
-
-
--- Save with Ctrl+s
-vim.keymap.set({ "n", "i" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 
 -- Fast jk to escape
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Escape insert mode" })
